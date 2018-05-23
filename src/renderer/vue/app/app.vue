35714a3ac8741ac91301d8b7d8f03e5e973083f3
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-05-23 15:56:54 +0700

    Update index.js: add Vue, scss, empty component

---
+++ src/renderer/vue/app/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app" 
+         class="col">
+         <ai-main/>
+    </div>
+</template>
+<script>
+import x from './x.vue';
+export default {
+    name: 'app',
+    components: {
+        ...'./main.vue',
+    },
+    mounted() {},
+    methods: {},
+};
+</script>
