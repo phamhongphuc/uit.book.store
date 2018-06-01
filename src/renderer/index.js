9cf48eeb9b48f28e40d5728ff100f0953b07b2e3
Unknown <sawa.kisei@gmail.com>
2018-06-01 15:50:08 +0700

    Add Vuex

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,7 +1,9 @@
+import Vue from 'vue';
+import store from './store';
+import app from './vue/app/app.vue';
 import './style/index.scss';
 
-import Vue from 'vue';
-import app from './vue/app/app.vue';
 new Vue({
+    store,
     render: h => h(app, { name: 'app', ref: 'app' }),
 }).$mount('#app');
