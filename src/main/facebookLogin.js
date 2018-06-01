0c8f7d6b20795e45c41c5cdccb6d92ae24470e5b
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-06-01 18:43:06 +0700

    Update Package, Add `url`

--- src/main/facebookLogin.js
+++ src/main/facebookLogin.js
@@ -1,5 +1,6 @@
 import { ipcMain, BrowserWindow } from 'electron';
 import fb from 'fb';
+import url from 'url';
 
 export default function(main) {
     ipcMain.on('facebook login', function(event, arg) {
@@ -8,9 +9,18 @@ export default function(main) {
             scopes: 'public_profile',
             redirect_uri: 'https://www.facebook.com/connect/login_success.html',
         };
-        var facebookAuthURL = `https://www.facebook.com/v2.8/dialog/oauth?client_id=${options.client_id}&redirect_uri=${
-            options.redirect_uri
-        }&response_type=token,granted_scopes&scope=${options.scopes}&display=popup`;
+        const facebookAuthURL = url.format({
+            protocol: 'https:',
+            hostname: 'www.facebook.com',
+            pathname: 'v2.8/dialog/oauth',
+            query: {
+                client_id: options.client_id,
+                scope: options.scopes,
+                redirect_uri: options.redirect_uri,
+                response_type: ['token', 'granted_scopes'],
+                display: 'popup',
+            },
+        });
 
         var authWindow = new BrowserWindow({
             width: 450,
@@ -24,12 +34,15 @@ export default function(main) {
         authWindow.loadURL(facebookAuthURL);
         authWindow.show();
         authWindow.webContents.on('did-get-redirect-request', function(authEvent, oldUrl, newUrl) {
+            console.log(1, authEvent);
+            console.log(2, oldUrl);
+            console.log(3, newUrl);
+
             var raw_code = /access_token=([^&]*)/.exec(newUrl) || null;
             var access_token = raw_code && raw_code.length > 1 ? raw_code[1] : null;
             var error = /\?error=(.+)$/.exec(newUrl);
-            console.log(access_token);
             event.returnValue = access_token;
-            authWindow.close();
+            // authWindow.close();
         });
     });
 }
